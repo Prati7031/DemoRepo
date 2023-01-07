1a3f6a33e5f07b08d167c0ba77786229a4c9ec2e
Pratik Sachani <122065602+Prati7031@users.noreply.github.com>
2023-01-06 20:27:51 -0500

    Update HomePage.java

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,4 +5,10 @@ public class HomePage {
 	public void homePage() {
 		System.out.println("home page");
 	}
+	
+	public void acerPage() {
+		System.out.println("Making acer page for submitting a pull request");
+	}
+	
 }
+
